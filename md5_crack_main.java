e0cec7e4a6dcdb121a40a4ec72a0d0e954c2c893
Ryan M <36736944+MatsonR98@users.noreply.github.com>
2021-03-18 13:03:49 -0400

    Update 3

--- md5_crack_main.java
+++ md5_crack_main.java
@@ -5,18 +5,53 @@ public class md5_crack_main {
 
     public static void main(String[] args){
         
+        String name;
+        String pass;
+
         //Mode to add a user to a text file
-        if(args[0].equals("-a") || args[0].equals("--adduser")){
+        if(args.length == 0){
+            System.out.println("Usage: -c or --cracker [user] | -a or --adduser [username] [password] | -p or --passwordcheck [password]");
+            System.exit(0);
+        }
+
+        else if(args[0].equals("-a") || args[0].equals("--adduser")){
 
             //Quit the program if the required fields are empty
             try{
-                String name = args[1];
-                String pass = args[2];
+                name = args[1];
+                pass = args[2];
             }
             catch(ArrayIndexOutOfBoundsException a){
-                System.out.print("Usage: -a|--adduser [username] [password]");
+                System.out.print("Usage: -a or --adduser [username] [password]");
                 System.exit(0);
             }
         }
+
+        else if(args[0].equals("-c") || args[0].equals("--cracker")){
+            
+            //Quit the program if the required fields are empty
+            try{
+                name = args[1];
+                pass = args[2];
+            }
+            catch(ArrayIndexOutOfBoundsException a){
+                System.out.print("Usage: -c or --cracker [user]");
+                System.exit(0);
+            }
+        }
+
+        else if(args[0].equals("-p") || args[0].equals("--passwordcheck")){
+
+            //Quit the program if the required fields are empty
+            try{
+                name = args[1];
+                pass = args[2];
+            } 
+            catch(ArrayIndexOutOfBoundsException a){
+                System.out.print("Usage: -p or --passwordcheck [password]");
+                System.exit(0);
+            }
+        }
+        
     }
 }
