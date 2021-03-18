7a091bd74263e846fb3c10bd4055feea50582a29
Ryan M <36736944+MatsonR98@users.noreply.github.com>
2021-03-18 10:38:17 -0400

    Added dictionary.txt
    Added dictionary file

---
+++ md5_crack_main.java
@@ -0,0 +1,12 @@
+import java.io.*;
+import java.util.*;
+
+public class md5_crack_main {
+
+    public static void main(String[] args){
+        
+        if(args[0] == "-c"){
+            
+        }
+    }
+}
