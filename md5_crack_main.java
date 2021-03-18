e9e4818ccce15b53e875bc26af90e81f83dff1a9
Ryan M <36736944+MatsonR98@users.noreply.github.com>
2021-03-18 10:51:40 -0400

    Update 2

--- md5_crack_main.java
+++ md5_crack_main.java
@@ -5,8 +5,19 @@ public class md5_crack_main {
 
     public static void main(String[] args){
         
-        if(args[0] == "-c"){
-            
+        //Mode to add a user to a text file
+        if(args[0].equals("-a") || args[0].equals("--adduser")){
+            try{
+                String name = args[1];
+                String pass = args[2];
+            }
+            catch(ArrayIndexOutOfBoundsException a){
+                System.out.print("Usage: -a|--adduser [username] [password]");
+                System.exit(0);
+            }
+            System.out.println(args[1]+" "+args[2]);
+
+            File outputfile = new File("user_store.txt");
         }
     }
 }
