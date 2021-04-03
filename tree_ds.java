7aaf5ee847ddcf93431b88b68b44a533010f61a2
Ryan M <36736944+MatsonR98@users.noreply.github.com>
2021-04-03 10:36:09 -0400

    Added tree_ds and tree_node classes

---
+++ tree_ds.java
@@ -0,0 +1,12 @@
+import java.util.*;
+import java.io.*;
+
+public class tree_ds{
+
+    private tree_node _head = null;
+    private tree_node _cur = null;
+
+    public void tree_ds(File wordlist){
+
+    }
+}
